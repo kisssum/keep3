895a6c5e365a79ef49edf9e507f3b19922c6d758
sea <1284700369@qq.com>
2020-12-23 19:04:35 +0800

    简介页改为关于我们

--- JianJiePager/script/top.js
+++ JianJiePager/script/top.js
@@ -2,10 +2,10 @@ $(()=> {
     $('#top').animate({
         marginTop: '0px'
     }, 2000, () => {
-        setTimeout(() => {
-            $('#top').animate({
-                marginTop: '-100px'
-            }, 2000)
-        }, 3000)
+        // setTimeout(() => {
+        //     $('#top').animate({
+        //         marginTop: '-100px'
+        //     }, 2000)
+        // }, 3000)
     })
 })
